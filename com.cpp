d4acac1622d1cb5e67e82b8f60ed321f2bd545db
Ry78Bc <ars9090kv@gmail.com>
2025-06-09 12:55:23 +0300

    xmm

--- com.cpp
+++ com.cpp
@@ -2,59 +2,75 @@
 #include <fstream>
 #include <cstring>
 
+#if defined(_WIN32) || defined(_WIN64)
+    #include <windows.h>
+    const char* path = "\\\\.\\COM%d";
+#elif defined(__linux__)
+    #include <unistd.h>
+    const char* path = "/dev/ttyACM%d";
+#else
+    #error "Unknown OS"
+#endif
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    ofstream com;
+    fstream com;
 
-    #if defined(_WIN32) || defined(_WIN64)
-        const char* path = "\\\\.\\COM%d";
-    #elif defined(__linux__)
-        const char* path = "/dev/ttyACM%d";
-    #else
-        #error "Unknown OS"
-    #endif
-
-    char buffer[25];
+    char buffer[20];
     char** mess;
     switch (argc) {
         case 1:
             cout << "\n No arg \n\n";
-            return 1;
+            return -1;
         case 2:
             for (int i=0; i<=9; i++) {
                 sprintf(buffer, path, i);
-                com.open(buffer);
+                com.open(buffer, ios::out | ios::in | ios::trunc);
                 if (com.is_open()) {
-                    sprintf(buffer, "\n Port %d found \n\n", i);
-                    cout << buffer;
+                    sprintf(buffer, "\n COM%d", i);
+                    mess = &argv[1];
                     break;
                 }
+                else {
+                    cout << "\n No ports available \n\n";
+                    return -2;
+                }
             }
-            mess = &argv[1];
         break;
         case 3: {
             int i = atoi(argv[1]);
             sprintf(buffer, path, i);
-            com.open(buffer);
+            com.open(buffer, ios::out | ios::in | ios::trunc);
             if (com.is_open()) {
-                sprintf(buffer, "\n Port %d open \n\n", i);
-                cout << buffer;
+                sprintf(buffer, "\n COM%d", i);
+                mess = &argv[2];
             }
-            mess = &argv[2];
+            else {
+                cout << "\n Сonnection error \n\n";
+                return -3;
+            }
         break; }
         default:
             cout << "\n Too match arg \n\n";
-            return 2;
+            return -4;
     }
 
-    if (com.is_open()) {
-        com << *mess << endl;
-        com.close();
-    } else {
-        cout << "\n Сonnection error \n\n";
-        return 3;
+    cout << buffer << " << " << *mess << endl;
+
+    // очистить файл открытый потоком com
+
+    com.clear();
+    com << *mess << endl;
+    com.flush();
+
+    string response;
+    while (getline(com, response)) {
+        if (response.empty()) continue;
+        cout << buffer << " >> " << response << "\n\n";
+        break;
     }
 
+    com.close();
     return 0;
 }
